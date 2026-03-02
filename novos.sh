f41918915c6676126fa0fa0dacb8296f9081f92d
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-02 21:37:46 +0000

    Update novos.sh

--- novos.sh
+++ novos.sh
@@ -1,18 +1,15 @@
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/onixsat/linux/refs/heads/main/novos.sh)" @ novos
+# bash -c "$(wget https://raw.githubusercontent.com/onixsat/linux/refs/heads/main/novos.sh)" @ novos
 #!/bin/bash
-
 sudo su
 sudo apt update -y
 wget https://raw.githubusercontent.com/onixsat/linux/refs/heads/main/editor/nginx/alterados/etc/nginx/nginx.conf
 wget https://raw.githubusercontent.com/onixsat/linux/refs/heads/main/editor/nginx/alterados/etc/nginx/sites-enabled/default
 wget https://raw.githubusercontent.com/onixsat/linux/refs/heads/main/editor/nginx/alterados/etc/nginx/sites-available/bo.conf
 wget https://raw.githubusercontent.com/onixsat/linux/refs/heads/main/editor/nginx/alterados/etc/nginx/sites-available/lb.conf
-# BACKUP ORIGINAIS
 sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bkp2
 sudo cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.bkp2
 sudo cp /etc/nginx/sites-available/bo.conf /etc/nginx/sites-available/bo.conf.bkp2
 sudo cp /etc/nginx/sites-available/lb.conf /etc/nginx/sites-available/lb.conf.bkp2
-# NOVOS
 sudo cp nginx.conf /etc/nginx/nginx.conf
 sudo cp default /etc/nginx/sites-enabled/default
 sudo cp bo.conf /etc/nginx/sites-available/bo.conf
