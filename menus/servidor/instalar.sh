e601349d402a580809e0f14da251f6937fd8588f
onixsat <163943278+onixsat@users.noreply.github.com>
2026-03-06 23:47:29 +0000

    Update instalar.sh

--- menus/servidor/instalar.sh
+++ menus/servidor/instalar.sh
@@ -1,4 +1,57 @@
 #!/bin/bash
+function esperar2(){
+  # Executar e esperar
+  # Run the command passed as 1st argument and shows the spinner until this is done
+  # @param String $1 the command to run
+  # @param String $2 the title to show next the spinner
+  # @param var $3 the variable containing the return code
+  CINZA="$(tput setaf 8)"
+  CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
+  CHECK_SYMBOL='\u2713'
+  X_SYMBOL='\u2A2F'
+  #local __resultvar=$3
+  local done=${3:-'Atualizado'}
+  local msg=$2
+
+  eval $1 >/tmp/execute-and-wait.log 2>&1 &
+  pid=$!
+  delay=0.05
+
+  frames=('\u280B' '\u2819' '\u2839' '\u2838' '\u283C' '\u2834' '\u2826' '\u2827' '\u2807' '\u280F')
+
+  echo "$pid" >"/tmp/.spinner.pid"
+
+  tput civis # Hide the cursor, it looks ugly :D
+  index=0
+  framesCount=${#frames[@]}
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    printf "${YELLOW}${frames[$index]}${NC} ${GREEN}${msg}${NC}"
+
+    let index=index+1
+    if [ "$index" -ge "$framesCount" ]; then
+      index=0
+    fi
+
+    printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+    sleep $delay
+  done
+
+  echo -e "\b\\r${CHECK_MARK}${CINZA} ${done}!   "
+echo -e ""
+  #printf " \b\n"
+  # Wait the command to be finished, this is needed to capture its exit status
+  #wait $!
+  #exitCode=$?
+  #if [ "$exitCode" -eq "0" ]; then
+  #  printf "${CHECK_SYMBOL} ${2}                                                                \b\n"
+  #else
+  #  printf "${X_SYMBOL} ${2}                                                                \b\n"
+  #fi
+
+  # Restore the cursor
+  #tput cnorm
+  #eval $__resultvar=$exitCode
+}
 
 # Update system packages
 log_info "Updating package lists and upgrading system..."
@@ -69,15 +122,20 @@ sudo iptables -I INPUT 1 -p tcp --dport 9000 -j ACCEPT
 
 read -n 1 -s -p "Press any key to continue 5"
 clear
+
+
 # Install Nginx UI
+titulo "Instalar pacotes do sistema..."
+
 echo "Installing Nginx U..." 
 log_info "Installing Nginx UI..."
-read -s -n 1 -p "Press any key to continue!"
 if ! command -v nginx-ui &> /dev/null; then
     bash -c "$(curl -fsSL https://cloud.nginxui.com/install.sh)" @ install
 else
     log_warn "Nginx UI already installed, skipping..."
 fi
+
+esperar "sleep 5" "Atualizando..." " ${WHITE} Atualizado!"
 
 read -n 1 -s -p "Press any key to continue 61"
 clear
